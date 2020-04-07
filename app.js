73e736ae6c1e406f172caaf91c93653ad61905ba
Alealvfer12 <60388111+Alealvfer12@users.noreply.github.com>
2020-04-07 15:57:52 -0500

    Primer commit
    Cree una clase la cual me permite conectarme con mi base de datos creada en postgres y cree i punto de entrada para mi API y las dependencias

---
+++ app.js
@@ -0,0 +1,24 @@
+// Importar libreria express
+const express = require("express");
+
+// Inicializar librería
+const app = express();
+app.use(express.json());
+
+// Endpoint
+app.get("/", (req, res) => {
+  res.send("Bienvenido a la API para guardar urls");
+});
+// IMPORTAR las rutas con los endpoints especificos
+const rutas_personas = require("./routes/personas");
+app.use(rutas_personas);
+
+const rutas_viviendas = require("./routes/viviendas");
+app.use(rutas_viviendas);
+
+// Puerto
+const port = 3000;
+// Levantar el servidor para escuchar los puertos
+app.listen(port, () => {
+  console.log(`Escuchando API en http://localhost:${port}`);
+});
